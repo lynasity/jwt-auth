1a1dd04fe94c923d40f543c00b53b585ac13633d
lynasity <1151864810@qq.com>
2017-02-16 00:25:07 +0800

    add guard configuration

---
+++ src/Providers/Auth/IlluminateAuthAdapter.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Providers\Auth;
+
+use Illuminate\Auth\AuthManager;
+
+class IlluminateAuthAdapter implements AuthInterface
+{
+    /**
+     * @var \Illuminate\Auth\AuthManager
+     */
+    protected $auth;
+    protected $guard;
+    /**
+     * @param \Illuminate\Auth\AuthManager  $auth
+     */
+    public function __construct(AuthManager $auth)
+    {
+        $this->auth = $auth;
+        $this->guard = config('jwt.guard',config('auth.defaults.guard'));
+    }
+
+    /**
+     * Check a user's credentials.
+     *
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function byCredentials(array $credentials = [])
+    {
+        return $this->auth->once($credentials);
+    }
+
+    /**
+     * Authenticate a user via the id.
+     *
+     * @param  mixed  $id
+     * @return bool
+     */
+    public function byId($id)
+    {
+        return $this->auth->guard($this->guard)->onceUsingId($id);
+    }
+
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return mixed
+     */
+    public function user()
+    {
+        return $this->auth->guard($this->guard)->user();
+    }
+}
